4d466728ed2a59da931070a2dd4d9ad843138e49
Matej Kopcik <56343549+MatejKopcik1@users.noreply.github.com>
2020-01-07 12:48:49 +0100

    Add files via upload

--- rgb.py
+++ rgb.py
@@ -7,13 +7,31 @@ s = 1600
 c=tkinter.Canvas(width = s, height = v, bg = "white")
 c.pack()
 
- 
+r = randrange(0,255)
+g = randrange(0,255)
+b = randrange(0,255)
+j = 1
+
 while True:
-    r = randrange(0,255)
-    g = randrange(0,255)
-    b = randrange(0,255)
-    farba = f"#{r:02x}{b:02x}{g:02x}"
+    c.delete("all")
+    farba = f"#{r:02x}{g:02x}{b:02x}"
+
+    b = b+j
+
+    if b == 255:
+        b = 0
+        g += j
+
+    if g == 255:
+        g = 0
+        b += j
+
+    if r == 255:
+        r = 0
+    
     c.create_rectangle(200,200,400,400,fill = farba)
-    c.after(100)
+    t = c.create_text(500,500,text=farba, font="Times 40")
+  
+    c.after(1)
     c.update()
     
